3e6b5e258952c2a587088cc531e7e18c21f37407
LizaDeLaRosa <31012822+LizaDeLaRosa@users.noreply.github.com>
2017-08-14 10:51:43 -0600

    MarchingBandStory.py
    This is my first project for computer programming. This stoory is incomplete, I still need to add all of my endings...

---
+++ Story.py
@@ -0,0 +1,6 @@
+user_reponse = 0 
+
+def begin_story():
+  print("You hear your alarm go off at 5 o'colck a.m. You need to start getting ready for band but you went to sleep late. Should you go to practice?")
+  print("Enter the number that corresponds with what you sleepily decided")
+  user_response = 
