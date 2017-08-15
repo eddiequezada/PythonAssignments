2730769dff13d4ad2aab2405dda1e6a88c3fac63
eddiequezada <31012686+eddiequezada@users.noreply.github.com>
2017-08-15 11:04:04 -0600

    Update Story.py
    if you don't like it its cool

--- Story.py
+++ Story.py
@@ -4,3 +4,4 @@ def begin_story():
   print("You hear your alarm go off at 5 o'colck a.m. You need to start getting ready for band but you went to sleep late. Should you go to practice?")
   print("Enter the number that corresponds with what you sleepily decided")
   user_response = 
+// you should include actions of the decision of going to practice or not, then what happens after that 
